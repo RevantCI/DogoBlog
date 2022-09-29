9fbc39bb1c2733589677021d7c82e3cc89be9b3a
Beenamol <beena.ashish@bridgeconn.com>
2022-09-29 12:13:23 +0530

    Remove git some modules

---
+++ Beena/src/Home.js
@@ -0,0 +1,17 @@
+import {useState} from 'react';
+const Home = () => {
+    // const [name, setName]= useState('Beena');
+    // const [age, setAge] = useState(33);
+    // const handleClick =() => {
+    //    setName('Ashish');
+    //    setAge(34); 
+    // }
+    return (  
+        <div className="Home">
+            <h2>Home Page</h2>
+            {/* <p>{name} is {age} years old</p>
+            <button onClick={handleClick}>Click Me</button> */}
+            </div>
+    );
+}
+export default Home;
