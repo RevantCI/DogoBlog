8e9bc9b89095af0603e3b4bc0bfba5dfcdcd2bf5
Beenamol <beena.ashish@bridgeconn.com>
2022-09-30 16:02:49 +0530

    added fech data with useEffect

--- Beena/src/Home.js
+++ Beena/src/Home.js
@@ -1,11 +1,7 @@
 import {useState, useEffect} from 'react';
 import BlogList from './BlogList';
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-            { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-            { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-            { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-          ]);
+    const [blogs, setBlogs] = useState(null);
 
           const [name, setName] = useState('mario');
           const handleDelete = (id) => {
@@ -14,15 +10,20 @@ const Home = () => {
         }
 
         useEffect(() => {
-           console.log("use effect ran");
-           console.log(name);
-        }, [name]);
+           fetch('http://localhost:8000/blogs')
+           .then(res =>{
+            return res.json();
+           })
+           .then(data => {
+            console.log(data);
+            setBlogs(data)
+           })
+        }, []);
     return (  
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
-            {/* <BlogList blogs={blogs.filter((blog) => blog.author === "mario")} title="Mario's Blogs!" /> */}
-            <button onClick={() => setName('Beena')}>change name</button>
-            <p>{name}</p>
+           {blogs && <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />}
+            
+           
             </div>
     );
 }
