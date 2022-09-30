0b2d87a9b4dc09acd215808a5c848194f5e71382
Beenamol <beena.ashish@bridgeconn.com>
2022-09-30 16:10:27 +0530

    feching data

--- Beena/src/Home.js
+++ Beena/src/Home.js
@@ -2,13 +2,7 @@ import {useState, useEffect} from 'react';
 import BlogList from './BlogList';
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
-
-          const [name, setName] = useState('mario');
-          const handleDelete = (id) => {
-             const newBlogs = blogs.filter(blog => blog.id !== id);
-             setBlogs(newBlogs);
-        }
-
+          
         useEffect(() => {
            fetch('http://localhost:8000/blogs')
            .then(res =>{
@@ -21,7 +15,7 @@ const Home = () => {
         }, []);
     return (  
         <div className="home">
-           {blogs && <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />}
+           {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
             
            
             </div>
