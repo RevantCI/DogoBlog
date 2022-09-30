882232b1ab5e473f480119de5c14d77a63d3d4b4
Beenamol <beena.ashish@bridgeconn.com>
2022-09-30 12:11:51 +0530

    Added useEffect dependencies

--- Beena/src/Home.js
+++ Beena/src/Home.js
@@ -6,6 +6,8 @@ const Home = () => {
             { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
             { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
           ]);
+
+          const [name, setName] = useState('mario');
           const handleDelete = (id) => {
              const newBlogs = blogs.filter(blog => blog.id !== id);
              setBlogs(newBlogs);
@@ -13,12 +15,14 @@ const Home = () => {
 
         useEffect(() => {
            console.log("use effect ran");
-           console.log(blogs);
-        });
+           console.log(name);
+        }, [name]);
     return (  
         <div className="home">
             <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
             {/* <BlogList blogs={blogs.filter((blog) => blog.author === "mario")} title="Mario's Blogs!" /> */}
+            <button onClick={() => setName('Beena')}>change name</button>
+            <p>{name}</p>
             </div>
     );
 }
