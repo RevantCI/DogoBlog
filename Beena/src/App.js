8b74373e978713de02c235c2b28295e8e59ba8a1
Beenamol <beena.ashish@bridgeconn.com>
2022-10-10 15:56:10 +0530

    added path

--- Beena/src/App.js
+++ Beena/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Home from './Home';
 import NavBar from './NavBar';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Create from './Create';
 
 function App() {
   
@@ -12,6 +13,9 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element = { <Home />}>
+          </Route> 
+          <Route path="/create" >
+            <Create />
           </Route> 
         </Routes>
         </Router>
